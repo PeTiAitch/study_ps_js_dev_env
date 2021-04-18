eca1171320c37a3808c216c12104e7d7e7e5f0af
plam <vvillbes@gmail.com>
2021-04-18 11:31:27 +0300

    11_13 Demo: Manipulating Data via JSON Server

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers, deleteUser } from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+// Populate table of users via API call.
+getUsers().then((result) => {
+  let usersBody = "";
 
-const result = (operation, num) => {
-  if(operation) {
-      return operation(num);
-    }
-    return num;
-}
+  result.forEach((user) => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`;
+  });
 
-function five(operation) {
-  return result(operation, 5);
-}
+  global.document.getElementById("users").innerHTML = usersBody;
 
-function seven(operation) {
-  if(operation) {
-    return operation(7);
-  }
-  return 7;
-}
+  const deleteLinks = global.document.getElementsByClassName("deleteUser");
 
-
-function minus(second) {
-  return (first) => first - second;
-}
-
-
-const a = seven(minus(five())); // must return 49
-
-console.log(a);
-
-
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, (link) => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
